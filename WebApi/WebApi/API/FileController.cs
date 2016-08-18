0280a07c00fb33f843b65226d1a3ba6801eddf86
Matt Brewerton <matt.brewerton@clickdatabases.co.uk>
2016-08-18 14:33:13 +0100

    Added test api contorllers

---
+++ WebApi/WebApi/API/FileController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApi.API
+{
+    [RoutePrefix("api/File")]
+    public class FileController : ApiController
+    {
+        [HttpGet]
+        [Route("Get")]
+        public HttpResponseMessage Get(string fileName)
+        {
+            return Request.CreateResponse(HttpStatusCode.NotImplemented);
+        }
+
+        [HttpPost]
+        [Route("Post")]
+        public HttpResponseMessage Post()
+        {
+            return Request.CreateResponse(HttpStatusCode.NotImplemented);
+        }
+    }
+}
---
+++ WebApi/WebApi/API/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.API
+{
+    public class MyObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class TestController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            var data = new List<MyObject> {
+                new MyObject { Id = 0, Name = "Hello" },
+                new MyObject { Id = 1, Name = "World" }
+            };
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+    }
+}
